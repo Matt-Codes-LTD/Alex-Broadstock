04e8147ee892d418b78f25840ca3fa80646bce56
MattyTurner95 <146556479+MattyTurner95@users.noreply.github.com>
2025-09-14 23:15:05 +0100

    Update barba-bootstrap.js

--- assets/v1/core/barba-bootstrap.js
+++ assets/v1/core/barba-bootstrap.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
         margin-top: -1px;
         transform: scaleY(0);
         transform-origin: 0% 100%;
-        background: #009ac9ff;
+        background: #000;
       `;
       grid.appendChild(div);
     }
