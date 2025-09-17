6db97a92dce375da93df58f78679c301db3ea85c
MattyTurner95 <146556479+MattyTurner95@users.noreply.github.com>
2025-09-17 22:35:31 +0100

    Update barba-bootstrap.js

--- assets/v1/core/barba-bootstrap.js
+++ assets/v1/core/barba-bootstrap.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
         margin-top: -1px;
         transform: scaleY(0);
         transform-origin: 0% 100%;
-        background: var(--swatch--brand-ink, #FDFCF3);
+        background: var(--swatch--brand-paper, #FDFCF3);
         will-change: transform;
         backface-visibility: hidden;
         contain: layout style paint;
